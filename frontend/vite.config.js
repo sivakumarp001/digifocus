ad9d670db23d6c01b71185b1b3d8cb484cd06b67
sivakumarp001 <sivakumarpsk001@gmail.com>
2026-04-05 22:52:35 +0530

    fix css build error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    transformer: 'postcss' // disable lightningcss
+  },
   server: {
     port: 5173,
     proxy: {
